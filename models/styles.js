4b000eb0073079ac2f744e723388abe3def9a34f
Jessica Ruiz <jeruiz1010@gmail.com>
2021-02-01 19:34:27 -0500

    restart of portfolio

--- models/styles.js
+++
@@ -1,8 +0,0 @@
-const Styles = (connection, Sequelize) => {
-  return connection.define('styles', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    type: { type: Sequelize.STRING, allowNull: false },
-  })
-}
-
-module.exports = Styles
